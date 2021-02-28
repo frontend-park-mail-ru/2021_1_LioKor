5b7894bbabeca3a61ca1ce84c2d1ccd59bb9393f
KoroLion <korolion31@yandex.ru>
2021-03-01 02:49:18 +0300

    templates: files.js module refactoring

--- src/modules/files.js
+++ src/modules/files.js
@@ -1,32 +1,47 @@
-export async function readImageAsDataURL(maxFileSizeMB = 10) {
-    return new Promise((resolve, reject) => {
-        let imageInput = document.createElement('INPUT');
+export const readImageAsDataURL = async (maxFileSizeMB = 10) => {
+    const createImageInput = (changeCallback) => {
+        const imageInput = document.createElement('INPUT');
         imageInput.type = 'file';
         imageInput.accept = 'image/png, image/jpeg, image/bmp';
         imageInput.style = 'display: none';
+        imageInput.addEventListener('change', changeCallback);
+        return imageInput
+    }
 
-        imageInput.addEventListener('change', () => {
-            if (imageInput.files.length == 1) {
-                let file = imageInput.files[0];
-                let typeSplitted = file.type.split('/');
-                if (typeSplitted.length == 0 || typeSplitted[0] != 'image') {
-                    reject(new Error('File is not an image!'));
-                } else if (file.size / 1048576 > maxFileSizeMB) {
-                    reject(new Error('File is bigger than allowed!'));
-                } else {
-                    const reader = new FileReader();
-                    reader.addEventListener('load', (e) => {
-                        resolve(e.target.result);
-                    });
-                    reader.addEventListener('error', (e) => {
-                        reject(new Error('Unable to read file!'));
-                    });
-                    reader.readAsDataURL(file);
-                }
-            } else {
-                reject(new Error('Wrong amount of files!'));
+    const getValidatedImage = (imageInput) => {
+        if (imageInput.files.length != 1) {
+            throw new Error('Wrong amount of files!');
+        }
+        let file = imageInput.files[0];
+        let typeSplitted = file.type.split('/');
+        if (typeSplitted.length == 0 || typeSplitted[0] != 'image') {
+            throw new Error('File is not an image!');
+        }
+        if (file.size / 1048576 > maxFileSizeMB) {
+            throw new Error('File is bigger than allowed!');
+        }
+
+        return file;
+    }
+
+    return new Promise((resolve, reject) => {
+        createImageInput((changeEvent) => {
+            let image;
+            try {
+                image = getValidatedImage(changeEvent.target);
+            } catch (err) {
+                reject(err);
+                return;
             }
-        });
-        imageInput.click();
+
+            const reader = new FileReader();
+            reader.addEventListener('load', (e) => {
+                resolve(e.target.result);
+            });
+            reader.addEventListener('error', (e) => {
+                reject(new Error('Unable to read file!'));
+            });
+            reader.readAsDataURL(image);
+        }).click();
     });
 }
