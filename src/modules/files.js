6e52f59d48a89ee303afc62509e8934d041083f2
KoroLion <korolion31@yandex.ru>
2021-03-01 09:17:13 +0300

    templates: eslint added

--- src/modules/files.js
+++ src/modules/files.js
@@ -1,47 +1,47 @@
 export const readImageAsDataURL = async (maxFileSizeMB = 10) => {
     const createImageInput = (changeCallback) => {
-        const imageInput = document.createElement('INPUT');
-        imageInput.type = 'file';
-        imageInput.accept = 'image/png, image/jpeg, image/bmp';
-        imageInput.style = 'display: none';
-        imageInput.addEventListener('change', changeCallback);
+        const imageInput = document.createElement('INPUT')
+        imageInput.type = 'file'
+        imageInput.accept = 'image/png, image/jpeg, image/bmp'
+        imageInput.style = 'display: none'
+        imageInput.addEventListener('change', changeCallback)
         return imageInput
     }
 
     const getValidatedImage = (imageInput) => {
-        if (imageInput.files.length != 1) {
-            throw new Error('Wrong amount of files!');
+        if (imageInput.files.length !== 1) {
+            throw new Error('Wrong amount of files!')
         }
-        let file = imageInput.files[0];
-        let typeSplitted = file.type.split('/');
-        if (typeSplitted.length == 0 || typeSplitted[0] != 'image') {
-            throw new Error('File is not an image!');
+        const file = imageInput.files[0]
+        const typeSplitted = file.type.split('/')
+        if (typeSplitted.length === 0 || typeSplitted[0] !== 'image') {
+            throw new Error('File is not an image!')
         }
         if (file.size / 1048576 > maxFileSizeMB) {
-            throw new Error('File is bigger than allowed!');
+            throw new Error('File is bigger than allowed!')
         }
 
-        return file;
+        return file
     }
 
     return new Promise((resolve, reject) => {
         createImageInput((changeEvent) => {
-            let image;
+            let image
             try {
-                image = getValidatedImage(changeEvent.target);
+                image = getValidatedImage(changeEvent.target)
             } catch (err) {
-                reject(err);
-                return;
+                reject(err)
+                return
             }
 
-            const reader = new FileReader();
+            const reader = new FileReader()
             reader.addEventListener('load', (e) => {
-                resolve(e.target.result);
-            });
+                resolve(e.target.result)
+            })
             reader.addEventListener('error', (e) => {
-                reject(new Error('Unable to read file!'));
-            });
-            reader.readAsDataURL(image);
-        }).click();
-    });
+                reject(new Error('Unable to read file!'))
+            })
+            reader.readAsDataURL(image)
+        }).click()
+    })
 }
