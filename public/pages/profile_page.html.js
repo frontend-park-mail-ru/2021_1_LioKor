0a57cb2b9f197c7abddf0b75febcb98f61519c56
KoroLion <korolion31@yandex.ru>
2021-03-10 02:44:23 +0300

    frontend: handlebars for profile

--- public/pages/profile_page.html.js
+++ public/pages/profile_page.html.js
@@ -1,18 +1,20 @@
 import { readImageAsDataURL } from '../modules/images.js';
 
+const DEFAULT_AVATAR_URL = '../images/default-avatar.jpg';
+
 const html = `
 <div class="profile">
     <div class="content">
         <div class="standalone-form profile">
             <div class="title">
                 <div class="avatar">
-                    <img id="avatarImage" src="../images/default-avatar.jpg">
+                    <img id="avatarImage" src="{{ avatarUrl }}">
                     <div id="avatarChange" class="cover">
                         Изменить
                     </div>
                 </div>
-                <div class="primary" id="username"></div>
-                <div class="secondary" id="email"></div>
+                <div class="primary" id="username">{{ username }}</div>
+                <div class="secondary" style="text-transform: lowercase;">{{ username }}@liokor.ru</div>
             </div>
             <div class="form">
                 <form id="editProfileForm">
@@ -20,18 +22,18 @@ const html = `
 
                     <div class="form-group" id="fullnameGroup">
                         <label>ПОЛНОЕ ИМЯ<span class="error-text" id="fullnameErrorText"></span></label>
-                        <input name="fullname" type="text" class="form-control" id="fullnameInput">
+                        <input name="fullname" type="text" class="form-control" value="{{ fullname }}">
                     </div>
                     <div class="form-group" id="reserveEmailGroup">
                         <label>ЗАПАСНОЙ E-MAIL<span class="error-text" id="reserveEmailErrorText"></span></label>
-                        <input name="reserveEmail" type="email" class="form-control" id="reserveEmailInput">
+                        <input name="reserveEmail" type="email" class="form-control" value="{{ reserveEmail }}">
                         <div class="muted">Необходимо будет подтвердить на старом и новом ящиках</div>
                     </div>
                     <div class="form-group">
                         <input type="submit" class="btn" value="Сохранить">
                     </div>
                     <div class="form-group">
-                        <LinkButton href="#" class="btn" id="changePasswordButton">Сменить пароль</LinkButton>
+                        <LinkButton href="/user/{{ username }}/password" class="btn" id="changePasswordButton">Сменить пароль</LinkButton>
                     </div>
                     <div class="form-group">
                         <linkButton href="/auth" class="btn btn-danger" id="logoutButton">Выйти</linkButton>
@@ -45,29 +47,31 @@ const html = `
 
 export async function source(element, app) {
     document.title = `${app.name} | Профиль`;
-    element.innerHTML = html;
-
-    const avatarImage = document.getElementById('avatarImage');
-    const avatarDataURL = document.getElementById('avatarDataURL');
 
     const response = await app.apiGet('/user');
     if (!response.ok) {
         app.goto('/auth');
         return;
     }
-
     const data = await response.json();
-    const { username, avatarUrl } = data;
+    let { username, avatarUrl } = data;
+    if (!avatarUrl) {
+        avatarUrl = DEFAULT_AVATAR_URL;
+    }
 
+    const template = Handlebars.compile(html);
+    element.innerHTML = template({
+        username: username,
+        fullname: data.fullname,
+        reserveEmail: data.reserveEmail
+    });
+
+    const avatarImage = document.getElementById('avatarImage');
+    const avatarDataURL = document.getElementById('avatarDataURL');
     if (avatarUrl) {
         avatarImage.src = avatarUrl;
         avatarDataURL.value = avatarUrl;
     }
-    document.getElementById('username').innerText = username;
-    document.getElementById('email').innerText = `${username.toLowerCase()}@liokor.ru`;
-    document.getElementById('fullnameInput').value = data.fullname;
-    document.getElementById('reserveEmailInput').value = data.reserveEmail;
-    document.getElementById('changePasswordButton').setAttribute('href', `${location.pathname}/${username}/password`);
 
     const editProfileForm = document.getElementById('editProfileForm');
     editProfileForm.addEventListener('submit', async (event) => {
