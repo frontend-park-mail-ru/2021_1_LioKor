cbbb1a4f48a201b342fbe9831f1c82f27c941718
KoroLion <korolion31@yandex.ru>
2021-03-10 03:16:58 +0300

    frontend: api url fix

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 import App from './modules/app.js';
 
 function main() {
-    let apiUrl = 'https://mail.liokor.ru/api';
+    let apiUrl = 'https://api.mail.liokor.ru';
     if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
         apiUrl = `http://${window.location.host}/api`;
     }
