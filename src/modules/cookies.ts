b4ad45410f68ff5eecb3de4e58627ceb84fb3742
KoroLion <korolion31@yandex.ru>
2021-04-22 01:15:43 +0300

    cookie module rewritten

--- src/modules/cookies.ts
+++ src/modules/cookies.ts
@@ -5,10 +5,17 @@
  * @returns {string} value of a cookie
  */
 export function getCookie(name: string): string {
-    const matches = document.cookie.match(`(^|;)\\s*${name}\\s*=\\s*([^;]+)`);
-    if (matches && matches.length > 0) {
-        return matches.pop() as string;
-    } else {
-        return '';
-    }
+    let csrfToken = '';
+
+    document.cookie.split(';').every((v) => {
+        const [key, ...value] = v.trim().split('=');
+
+        if (key === name) {
+            csrfToken = value.join('=');
+            return false;
+        }
+        return true;
+    })
+
+    return csrfToken;
 }
