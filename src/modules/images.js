cd54f10d3fac1a7131615b268491fc4a92d8e227
KoroLion <korolion31@yandex.ru>
2021-04-21 03:22:39 +0300

    refactoring

--- src/modules/images.js
+++ src/modules/images.js
@@ -1,6 +1,9 @@
+const DEFAULT_ACCEPT = 'image/png, image/jpeg, image/bmp';
 const DEFAULT_SIZE = 256;
 const DEFAULT_MAX_FILE_SIZE_MB = 10;
 const MB = 1024 * 1024;
+
+const TEMP_EL_ID = 'filesImageInput';
 
 const createCanvas = (width, height) => {
     const canvas = document.createElement('canvas');
@@ -15,21 +18,26 @@ const createCanvas = (width, height) => {
 /**
  * Opens user file selection (with filter to images) dialog and returns dataURL of selected image
  *
+ * @param accept
  * @param {number} size size of resulting image (square size x size)
  * @param {number} maxFileSizeMB maximum allowed file size
  * @returns {string} Data url of image selected by user
  */
-export const readImageAsDataURL = async (size = DEFAULT_SIZE, maxFileSizeMB = DEFAULT_MAX_FILE_SIZE_MB) => {
+export const readImageAsDataURL = async (
+    accept = DEFAULT_ACCEPT,
+    size = DEFAULT_SIZE,
+    maxFileSizeMB = DEFAULT_MAX_FILE_SIZE_MB
+) => {
     const createImageInput = (changeCallback) => {
         let hasInp = true;
-        let imageInput = document.getElementById('filesImageInput');
+        let imageInput = document.getElementById(TEMP_EL_ID);
         if (imageInput === null) {
             hasInp = false;
             imageInput = document.createElement('input');
         }
-        imageInput.id = 'filesImageInput';
+        imageInput.id = TEMP_EL_ID;
         imageInput.type = 'file';
-        imageInput.accept = 'image/png, image/jpeg, image/bmp';
+        imageInput.accept = accept;
         imageInput.addEventListener('change', changeCallback);
 
         imageInput.style.display = 'none';
