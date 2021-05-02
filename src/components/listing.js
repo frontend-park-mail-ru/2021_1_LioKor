fab0ae23e6a6b71c447970d7b3070ebf2ea1d7fd
Sergey Tyapkin <Tyapkin2002@mail.ru>
2021-05-02 16:25:14 +0300

    Сделал Listing

--- src/components/listing.js
+++ src/components/listing.js
@@ -1,51 +1,95 @@
+export const plugStates = {
+    none: 0,
+    loading: 1,
+    end: 2,
+    offline: 3
+};
+
 export class Listing {
-    block = undefined;
+    block;
     elements = [];
+
+    scrollHandler;
+    clickElementHandler;
+
+    selectedElem;
+    activeElem;
+
+    plugTop = plugStates.none;
+    plugBottom = plugStates.none;
 
     constructor(block) {
         this.block = block;
     }
 
+    findById(id) {
+        return this.elements.find(elem => elem.id === id);
+    }
+
     setScrollHandlers(scrollTopHandler, scrollBottomHandler, scrollTopOffset = 0, scrollBottomOffset = 0) {
-        this.block.addEventListener('scroll', (event) => {
+        this.scrollHandler = (event) => {
             if (this.block.scrollTop < scrollTopOffset) {
                 scrollTopHandler();
             }
             if (this.block.scrollTop + this.block.clientHeight < this.block.scrollHeight - scrollTopOffset) {
                 scrollBottomHandler();
             }
-        });
+        };
+
+        this.block.addEventListener('scroll', this.scrollHandler);
     }
 
     setClickElementHandler(handler) {
-        this.block.addEventListener('click', (event) => {
+        this.clickElementHandler = (event) => {
             handler(event.currentTarget.id);
-        });
+        };
+
+        this.block.addEventListener('click', this.clickElementHandler);
     }
 
-    add(element) {
-        this.elements.push({ elem: element, id: this.elements.length });
+    push(element) {
+        this.elements.push(element);
+    }
+
+    unshift(element) {
+        this.elements.unshift(element);
     }
 
     delete(id) {
         const index = this.elements.findIndex(elem => elem.id === id);
-        this.elements[index].elem.remove();
+        this.elements[index].remove();
+        this.elements[index].removeEventListener(this.clickElementHandler);
         this.elements.splice(index, 1);
     }
 
     getElementsHeight() {
         let height = 0;
-        this.elements.forEach((element) => { height += element.elem.clientHeight; });
+        this.elements.forEach((element) => { height += element.clientHeight; });
         return height;
     }
 
     clear() {
-        this.elements.forEach((elem) => { elem.elem.remove(); });
+        this.elements.forEach((elem) => {
+            elem.remove();
+            elem.removeEventListener(this.clickElementHandler);
+        });
     }
 
     redraw() {
         this.elements.forEach((elem) => {
-            console.log(elem);
+            this.block.appendChild(elem);
         });
+    }
+
+    setSelected(id) {
+        this.selectedElem.classList.remove('selected');
+        this.selectedElem = this.findById(id);
+        this.selectedElem.classList.add('selected');
+    }
+
+    setActive(id) {
+        this.activeElem.classList.remove('active');
+        this.activeElem = this.findById(id);
+        this.activeElem.classList.add('active');
     }
 }
--- src/views/messages.html.js
+++ src/views/messages.html.js
@@ -48,7 +48,7 @@ const html = `
                 <svg class="svg-button floatright" id="refresh-connection-button" xmlns="http://www.w3.org/2000/svg" style="transition: all ease-in-out 1s; transform: rotate(0deg)"><g transform="scale(0.04) translate(60, 0)"><path d="M112.156,97.111c72.3-65.4,180.5-66.4,253.8-6.7l-58.1,2.2c-7.5,0.3-13.3,6.5-13,14c0.3,7.3,6.3,13,13.5,13    c0.2,0,0.3,0,0.5,0l89.2-3.3c7.3-0.3,13-6.2,13-13.5v-1c0-0.2,0-0.3,0-0.5v-0.1l0,0l-3.3-88.2c-0.3-7.5-6.6-13.3-14-13    c-7.5,0.3-13.3,6.5-13,14l2.1,55.3c-36.3-29.7-81-46.9-128.8-49.3c-59.2-3-116.1,17.3-160,57.1c-60.4,54.7-86,137.9-66.8,217.1    c1.5,6.2,7,10.3,13.1,10.3c1.1,0,2.1-0.1,3.2-0.4c7.2-1.8,11.7-9.1,9.9-16.3C36.656,218.211,59.056,145.111,112.156,97.111z"/><path d="M462.456,195.511c-1.8-7.2-9.1-11.7-16.3-9.9c-7.2,1.8-11.7,9.1-9.9,16.3c16.9,69.6-5.6,142.7-58.7,190.7    c-37.3,33.7-84.1,50.3-130.7,50.3c-44.5,0-88.9-15.1-124.7-44.9l58.8-5.3c7.4-0.7,12.9-7.2,12.2-14.7s-7.2-12.9-14.7-12.2l-88.9,8    c-7.4,0.7-12.9,7.2-12.2,14.7l8,88.9c0.6,7,6.5,12.3,13.4,12.3c0.4,0,0.8,0,1.2-0.1c7.4-0.7,12.9-7.2,12.2-14.7l-4.8-54.1    c36.3,29.4,80.8,46.5,128.3,48.9c3.8,0.2,7.6,0.3,11.3,0.3c55.1,0,107.5-20.2,148.7-57.4    C456.056,357.911,481.656,274.811,462.456,195.511z"/></g></svg>
             </div>
 
-            <div class="body fullheight fullwidth table-rows scrollable" id="messages-field">
+            <div class="body fullheight fullwidth table-rows scrollable" id="messages-listing">
             </div>
         </div>
 
@@ -106,7 +106,9 @@ export async function handler(element, app) {
     const mainFolderName = 'Все входящие';
 
     // --- HTML elements
-    const dialoguePreviewsGroup = document.getElementById('dialogues-listing');
+    const dialoguesListing = new Listing(document.getElementById('dialogues-listing'));
+    const messagesListing = new Listing(document.getElementById('messages-listing'));
+
     const dialogueHeader = document.getElementById('dialogue-header-title');
     const dialogueTime = document.getElementById('dialogue-header-time');
 
@@ -114,7 +116,6 @@ export async function handler(element, app) {
     const messagesColumn = document.getElementById('messages-column');
 
     const backToDialoguesButton = document.getElementById('header-title-button');
-    const messagesField = document.getElementById('messages-field');
     const messagesFooter = document.getElementById('messages-footer');
 
     const connectionsInfo = document.getElementsByClassName('connection-info');
@@ -127,13 +128,6 @@ export async function handler(element, app) {
 
     const themeInput = document.getElementById('theme-input');
     const messageInput = document.getElementById('message-input');
-    // --- Plug-elements
-    const plugStates = {
-        loading: 0,
-        end: 1,
-        none: 2,
-        offline: 3
-    };
     // --- Big containers
     const dialogues = {
         storage: [],
