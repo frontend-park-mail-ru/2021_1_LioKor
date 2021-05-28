028be71c8514e529c4b95aa9c76bba4f98ab1214
Sergey Tyapkin <Tyapkin2002@mail.ru>
2021-05-28 23:39:54 +0300

    Добавил динамическое изменение фона и скрытие логотипа

--- src/modules/renderer.js
+++ src/modules/renderer.js
@@ -1,13 +1,24 @@
+const defaultBackground = '#161923';
+const bodyElement = document.querySelector('body');
+const logoElement = document.querySelector('header');
 /**
  * Renders page with animation
  *
  * @param {object} target html element to render in
  * @param {object} handler function of a page (from .html.js)
  * @param {object} app object of a main App class
+ * @param background
+ * @param hasLogo
  * @returns {Promise} promise that will be resolved, when page is displayed
  */
-export function render(target, handler, app) {
+export function render(target, handler, app, background, hasLogo = true) {
     return new Promise((resolve, reject) => {
+        console.log(background, hasLogo);
+        if (!background) {
+            background = defaultBackground;
+        }
+        bodyElement.style.background = background;
+        logoElement.style.maxHeight = hasLogo ? '100px' : '0';
         const el = document.getElementById(target);
         try {
             el.style.opacity = 0;
